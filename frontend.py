e5fd5e61d32abc420e12627e8a4a29f0632d052e
jtan573 <jtan573@e.ntu.edu.sg>
2024-07-19 14:37:05 +0800

    frontend

--- frontend.py
+++ frontend.py
@@ -1,24 +1,30 @@
 import gradio as gr
 import soundfile as sf
+import requests
+from scipy.io.wavfile import write
+import os
+from dotenv import load_dotenv
 
-def greet(name, intensity):
-    return "Hello, " + name + "!" * int(intensity)
+load_dotenv()
 
-def translate_audio_to_text(audio):
-    # Read the audio file
-    speech, rate = sf.read(audio.name)
-    
-    # Process the audio
-    
-    # Get the translated output
-    transcription = "Here is your translation..."
+def predict(audio):
+    sample_rate, audio_data = audio
+    temp_file = "temp_audio.wav"
+    write(temp_file, sample_rate, audio_data)
 
-    return transcription
+    # Send the file to the FastAPI server
+    with open(temp_file, "rb") as f:
+        fastapi_server_ip = os.getenv("FASTAPI_SERVER_IP")
+        fastapi_server_port = os.getenv("FASTAPI_SERVER_PORT")
+        response = requests.post(f"{fastapi_server_ip}:{fastapi_server_port}/process/", files={"file": f})
+
+    print(response)
+    return response.json()["generated_output"][0][0]
 
 # Create the Gradio interface
 interface = gr.Interface(
-    fn = translate_audio_to_text, 
-    inputs = gr.Audio(sources="upload", type="filepath"), 
+    fn = predict,
+    inputs = gr.Audio(sources="upload", type="numpy"), 
     outputs = "text",
     title = "Audio to Text Translator",
     description = "Upload a .wav file and get the English text translation."
